78a18b933fd42da6d88398808d7e98f5f1d40588
Siddhanth Sisodia <sisodiasiddhanth@gmail.com>
2023-03-20 17:49:17 +0530

    First time adding all Java programs to Git

---
+++ Nibiru/src/Basicprograms/NestedForLoop.java
@@ -0,0 +1,18 @@
+package Basicprograms;
+
+public class NestedForLoop {
+
+	public static void main(String[] args) {
+		int i ;
+		// First for loop will be for creating spaces
+		for(int j = 5;j>0;j--)
+		{
+			for(i = 5; i>0;i--)
+			{
+				System.out.print("hi");
+			}
+			
+			System.out.println(" ");
+		}
+	}
+}
