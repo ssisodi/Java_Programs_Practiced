add7c01d36bffc553b44c8bd72e2d451c55e94bb
Siddhanth Sisodia <sisodiasiddhanth@gmail.com>
2023-05-01 16:22:55 +0530

    1st May 2023

--- Nibiru/src/batman/Array4.java
+++ Nibiru/src/batman/Array4.java
@@ -1,10 +1,28 @@
 package batman;
 
+import java.util.Scanner;
+
 public class Array4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("How many values:");
+		int a = sc.nextInt();
 
+		char arr[] = new char[a]; 
+
+		System.out.println("Enter those values");
+		String ben = sc.next();
+
+		for(int i=0;i<a;i++)
+		{
+			arr[i] = ben.charAt(i);
+		}
+		for(int j=a;j>0;j--)
+		{
+			System.out.println(arr[j]);
+		}
 	}
-
 }
+
+
